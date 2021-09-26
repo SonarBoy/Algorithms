d9ceb601f6e2f13aa4ef6c0df93946884b5fabe5
SonarBoy <Joshua.L99.C@gmail.com>
2021-09-26 03:43:57 -0400

    The first attempt before the copy. TY INRI + MM.

--- src/elementary/Solver.java
+++ src/elementary/Solver.java
@@ -1,19 +1,28 @@
 package elementary;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Scanner;
 
+
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+
 
 public class Solver {
-	
+
 	private int moves;
 	private MinPQ<SearchNode> openList;
+	private boolean isSolve;
+	private Stack<Board> solution;
+	
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
+    	
+    	
     	
     	openList = new MinPQ<SearchNode>(
     			(Comparator<SearchNode>) new Comparator<SearchNode>() {    
@@ -25,7 +34,13 @@ public class Solver {
     					}else if(s1.priority() > s2.priority()) {
     						return 1;
     					}else {
-    						return 0;
+    						if(s1.current().manhattan() < s2.current().manhattan()) {
+    							return -1;
+    						}else if(s1.current().manhattan() > s2.current().manhattan()) {
+    							return 1;
+    						}else {
+    							return 0;
+    						}
     					}
     					
     				}
@@ -42,8 +57,15 @@ public class Solver {
     					}else if(s1.priority() > s2.priority()) {
     						return 1;
     					}else {
-    						return 0;
+    						if(s1.current().manhattan() < s2.current().manhattan()) {
+    							return -1;
+    						}else if(s1.current().manhattan() > s2.current().manhattan()) {
+    							return 1;
+    						}else {
+    							return 0;
+    						}
     					}
+    					
     					
     				}
     			});
@@ -54,60 +76,148 @@ public class Solver {
     	
     	openList.insert(currentNode);
     	
-    	while(!currentNode.current().isGoal() && !openList.isEmpty()) {
+    	while(!openList.isEmpty()) {
+//    		System.out.println("Current");
+//    		System.out.println("Manhattan: " + currentNode.current().manhattan());
+//    		System.out.println("Hamming: "+ currentNode.current().hamming());
+//    		System.out.println(currentNode.current().toString());
     		
+    		
+    		closedList.insert(currentNode);
+    		
+    		if(currentNode.current().isGoal()) {
+    			this.moves = currentNode.moves;
+    			this.isSolve = true;
+    			break;
+    		}
     		
     		
     		Iterable<Board> queue = currentNode.current().neighbors();
     		Iterator<Board> iter = queue.iterator();
-    		Iterator<SearchNode> openListIterator = openList.iterator();
-    		Iterator<SearchNode> closedListIterator = closedList.iterator();
+    		
+    		
+    		
+//    		System.out.println("Neighbours");
+//    		System.out.println("---------------------");
+    		SearchNode node = null;
+    		
+    		
+    		
+    		this.moves = currentNode.moves;
+    		int nextMoves = this.moves + 1;
+    		
     		
     		for(;iter.hasNext();) {
-    			Board checkingNode = iter.next(); 
+    			Board checkingNode = iter.next();
+    			
+    			
+    			node = new SearchNode(checkingNode,nextMoves,currentNode);
+    			
+    			
+//    			System.out.println(checkingNode.toString());
+//    			System.out.println("Manhattan: "+checkingNode.manhattan());
+//    			System.out.println("Hamming: "+checkingNode.hamming());
+//    			System.out.println("Moves: "+ nextMoves);
+    			
     			
     			boolean inOpen = false;
     			boolean inClosed = false;
     			
-    			while(openListIterator.hasNext()) {
-    				if(openListIterator.next().current().equals(checkingNode)) {
-    					inOpen = true;
-    				}
-    			}
     			
+    			SearchNode runner = currentNode;
+//    			while(runner.prev() != null) {
+//    				if(runner.current().equals(checkingNode)) {
+//    					inClosed = true;
+//    					break;
+//    				}
+//    				runner = runner.prev();
+//    			}
+    			
+    			Iterator<SearchNode> closedListIterator = closedList.iterator();
     			while(closedListIterator.hasNext()) {
-    				if(closedListIterator.next().current().equals(checkingNode)) {
+    				
+    				SearchNode closedNode = closedListIterator.next();
+    				
+    				if(closedNode.current().equals(checkingNode)) {
+    					
     					inClosed = true;
+    					//System.out.println("Closed");
+    					break;
     				}
+
     			}
     			
-    			if(!inOpen && !inClosed) {
-    				this.moves++;
-    				
-    				SearchNode node = new SearchNode(checkingNode,this.moves,currentNode);
-    				
-    				openList.insert(node);
-    				
+    			if(!inClosed) {
+    				Iterator<SearchNode> openListIterator = openList.iterator();
+        			while(openListIterator.hasNext()) {
+        				
+        				SearchNode openNode = openListIterator.next();
+        				
+        				if(openNode.current().equals(checkingNode)) {
+        					
+        					inOpen = true;
+        					//System.out.println("Open");
+        					break;
+        				}
+        				
+        			
+        			}
     			}
+
     			
+    				if(!inClosed && !inOpen) {
+    					
+    						openList.insert(node);
+    				
+    				}
+    				
     			
     		}
+//    		System.out.println("---------------------");
     		
-    		//System.out.println(currentNode.current().toString());
+    		currentNode = openList.delMin();
     		
+//    		Iterator<SearchNode> openListIterator = openList.iterator();
+//			while(openListIterator.hasNext()) {
+//				System.out.println(openListIterator.next().current().toString());
+//			}
     		
+
     		
-    		
-    		closedList.insert(currentNode);
-    		currentNode = openList.min();
-    		System.out.println(openList.delMin().current().toString());
+    		//System.out.println(openList.delMin().current().toString());
     	}
+    	
+    	
+    	//System.out.println("Moves: "+this.moves);
+    	if(currentNode.current().isGoal() == false) {
+    		this.isSolve = false;
+    		System.out.println("Unsolved");
+    	}else {
+    		this.isSolve = true;
+    		System.out.println("Solved");
+    		int runner = 0;
+        	
+        	//System.out.println("Runner : " + runner);	
+    		solution = new Stack<Board>();
+    		
+    		while(currentNode.prev() != null) {
+        		runner++;
+        		System.out.println(currentNode.current().toString());
+        		solution.push(currentNode.current());
+        		currentNode = currentNode.prev();
+        		
+        	}
+        	System.out.println(currentNode.current().toString());
+    		solution.push(currentNode.current());
+    	}
+    	
+    	
     	
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
-    	return false;
+    	return this.isSolve;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
@@ -117,7 +227,11 @@ public class Solver {
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution(){
-    	return null;
+    	if(!this.isSolve) {
+    		return null;
+    	}
+    		
+    	return solution;
     }
     
     private class SearchNode {
@@ -144,6 +258,10 @@ public class Solver {
     	
     	public int priority() {
     		return this.priority;
+    	}
+    	
+    	public int moves() {
+    		return this.moves;
     	}
     }
     
